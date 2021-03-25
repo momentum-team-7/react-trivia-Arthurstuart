86005bbcce30c1026212041bbbe5efc413fa35d9
arthurstuart <arthur.stuart10@gmail.com>
2021-03-25 18:56:16 -0400

    got past map

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,19 +1,19 @@
 export default function Category (categories, setselectedCategory) {
 return (
-    <section>
+<div className="category_list">
     <h2>Choose a Category</h2>
-<ul className="category_list">
-{categories.map((category, idx) => (
-    <li key={idx}>
-        <button className="category-button" onClick={() => setselectedCategory(category)}>
-            {category.name}
-        </button>
-        </li>
-))
-}
+        <ul className="list_of_categories">
+            {categories.map((category, idx) => (
+                <li key={category.category}>      
+                <button
+                    classname="choose-category"
+                    onClick={() => setselectedCategory(category)}
+                        >
+                        {category.results}
+                        </button>
+                </li>
+            ))}
+      </ul>
+</div>
 )
-</ul>
-    </section>
-)
 }
-
